6e2e2ef9a9304bc8306a46a698db879900ee36c9
Hanna Kebedom <hanna.kebedom@shopify.com>
2021-12-14 20:08:05 -0500

    Finished DDL.sql file. Need to test this further to make sure that there are no typos and TAs can create my database seamlessly. Also want to revisit the constraints on some of the fields and 'on delete cascade' vs 'on delete null' for the foreign keys

--- DDL.sql
+++ DDL.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Customer
-	(customer_id int AUTO_INCREMENT,
+	(customer_id SERIAL,
 	 name       varchar(50),
 	 address    varchar(40),
      card_number varchar(16),
@@ -9,7 +9,7 @@ CREATE TABLE Customer
 	);
 
 CREATE TABLE Owner
-    (owner_id int AUTO_INCREMENT,
+    (owner_id   SERIAL,
 	 name       varchar(50),
 	 address    varchar(40),
 	 PRIMARY KEY (owner_id)
@@ -22,12 +22,176 @@ CREATE TABLE Account
 	 PRIMARY KEY (username)
 	);
 
-CREATE TABLE Owner
-    (owner_id int AUTO_INCREMENT,
-	 name       varchar(50),
-	 address    varchar(40),
-	 PRIMARY KEY (owner_id)
+CREATE TABLE Orders
+    (order_id   SERIAL,
+	 order_date       date,
+	 status     varchar(40),
+	 PRIMARY KEY (order_id)
 	);
+
+CREATE TABLE Cart
+    (cart_id SERIAL,
+	 PRIMARY KEY (cart_id)
+	);
+
+CREATE TABLE Book
+    (isbn int UNIQUE,
+     title varchar(40),
+     description text,
+     pages int,
+     year_published int,
+	 PRIMARY KEY (isbn)
+	);
+
+CREATE TABLE Genre
+    (name varchar(20) UNIQUE,
+	 PRIMARY KEY (name)
+	);
+
+CREATE TABLE Publisher
+    (publisher_id SERIAL,
+     name varchar(50),
+     address varchar(40),
+     email varchar(20),
+     phone varchar(10),
+     bank_account_no varchar(10),
+	 PRIMARY KEY (publisher_id)
+	);
+
+CREATE TABLE Author
+    (author_id SERIAL,
+     name varchar(50),
+     bio text,
+	 PRIMARY KEY (author_id)
+	);
+
+CREATE TABLE Warehouse
+    (number SERIAL,
+     address varchar(40),
+     phone varchar(10),
+	 PRIMARY KEY (number)
+	);
+
+CREATE TABLE Store
+    (store_id SERIAL,
+     name varchar(50),
+     address varchar(10),
+	 PRIMARY KEY (store_id)
+	);
+
+CREATE TABLE Bank_Account
+    (account_number SERIAL,
+     balance int,
+	 PRIMARY KEY (account_number)
+	);
+
+CREATE TABLE Owner_Account
+    (username varchar(20),
+     owner_id int,
+	 PRIMARY KEY (username),
+     FOREIGN KEY (username) references Account
+		on delete cascade,
+     FOREIGN KEY (owner_id) references Owners
+		on delete cascade
+	);
+
+CREATE TABLE Customer_Account
+    (username varchar(20),
+     customer_id int,
+	 PRIMARY KEY (username),
+     FOREIGN KEY (username) references Account
+		on delete cascade,
+     FOREIGN KEY (customer_id) references Customer
+		on delete cascade
+	);
+
+CREATE TABLE Places
+    (order_id    int,
+     username    varchar(20),
+	 PRIMARY KEY (order_id),
+     FOREIGN KEY (order_id) references Orders
+		on delete cascade,
+     FOREIGN KEY (username) references Account
+		on delete cascade
+	);
+
+CREATE TABLE Manages
+    (cart_id    int,
+     username   varchar(20),
+	 PRIMARY KEY (cart_id),
+     FOREIGN KEY (cart_id) references Cart
+		on delete cascade,
+     FOREIGN KEY (username) references Account
+		on delete cascade
+	);
+
+CREATE TABLE Contains
+    (cart_id    int,
+     isbn       int UNIQUE,
+     quantity   int,  
+	 PRIMARY KEY (cart_id, isbn),
+     FOREIGN KEY (cart_id) references Cart
+		on delete cascade,
+     FOREIGN KEY (isbn) references Book
+		on delete cascade
+	);
+
+CREATE TABLE Writes
+    (author_id  int,
+     isbn       int UNIQUE,
+	 PRIMARY KEY (author_id, isbn),
+     FOREIGN KEY (author_id) references Author
+		on delete cascade,
+     FOREIGN KEY (isbn) references Book
+		on delete cascade
+	);
+
+CREATE TABLE Publishes
+    (publisher_id  int,
+     isbn          int UNIQUE,
+	 PRIMARY KEY (publisher_id, isbn),
+     FOREIGN KEY (publisher_id) references Publisher
+		on delete cascade,
+     FOREIGN KEY (isbn) references Book
+		on delete cascade
+	);
+
+CREATE TABLE Warehouse_Book
+    (number  int,
+     isbn    int UNIQUE,
+     quantity int,
+	 PRIMARY KEY (number, isbn),
+     FOREIGN KEY (number) references Warehouse
+		on delete cascade,
+     FOREIGN KEY (isbn) references Book
+		on delete cascade
+	);
+
+CREATE TABLE Store_Book
+    (store_id  int,
+     isbn      int UNIQUE,
+     quantity  int,
+	 PRIMARY KEY (store_id, isbn),
+     FOREIGN KEY (store_id) references Store
+		on delete cascade,
+     FOREIGN KEY (isbn) references Book
+		on delete cascade
+	);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
